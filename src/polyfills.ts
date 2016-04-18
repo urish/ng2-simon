e845a30b9f51815ce8bcf3058bd320025a434f0d
Uri Shaked <uri@salsa4fun.co.il>
2016-04-18 14:10:00 +0000

    Initial commit (clone angular2-webpack-starter)

---
+++ src/polyfills.ts
@@ -0,0 +1,25 @@
+// Polyfills
+// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
+
+// import 'ie-shim'; // Internet Explorer
+// import 'es6-shim';
+// import 'es6-promise';
+// import 'es7-reflect-metadata';
+
+// Prefer CoreJS over the polyfills above
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+require('zone.js/dist/zone');
+
+if ('production' === ENV) {
+  // Production
+
+
+} else {
+  // Development
+
+  Error.stackTraceLimit = Infinity;
+
+  require('zone.js/dist/long-stack-trace-zone');
+
+}
