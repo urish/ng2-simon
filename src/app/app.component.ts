de3192635cd59d39c672dc0a60d1efe0ec11fe30
Uri Shaked <uri@salsa4fun.co.il>
2016-04-18 14:09:19 +0100

    add basic game logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@
  * Angular 2 decorators and services
  */
 import {Component, ViewEncapsulation} from 'angular2/core';
-import {RouteConfig, Router} from 'angular2/router';
 
-import {Home} from './home';
-import {AppState} from './app.service';
-import {RouterActive} from './router-active';
+import {SimonGame} from './simon/SimonGame';
 
 /*
  * App Component
@@ -14,13 +11,17 @@ import {RouterActive} from './router-active';
  */
 @Component({
   selector: 'app',
-  pipes: [ ],
-  providers: [ ],
-  directives: [ RouterActive ],
+  pipes: [],
+  providers: [],
+  directives: [SimonGame],
   encapsulation: ViewEncapsulation.None,
   styles: [`
     body {
       margin: 0;
+      background: #333;
+      color: #ddd;
+      font-size: 24px;
+      text-align: center;
     }
     md-toolbar ul {
       display: inline;
@@ -35,66 +36,25 @@ import {RouterActive} from './router-active';
     md-toolbar li.active {
       background-color: lightgray;
     }
+    simon-game {
+      display: block;
+      margin-top: 40px;
+    }
   `],
   template: `
     <header>
       <md-toolbar color="primary">
-        <span>{{ name }}</span>
-        <nav>
-          <ul>
-            <li router-active>
-              <a [routerLink]=" ['Index'] ">Index</a>
-            </li>
-            |
-            <li router-active>
-              <a [routerLink]=" ['Home'] ">Home</a>
-            </li>
-            |
-            <li router-active>
-              <a [routerLink]=" ['About'] ">About</a>
-            </li>
-          </ul>
-        </nav>
+        <span>Angular2 Simon</span>
       </md-toolbar>
     </header>
 
     <main>
-      <router-outlet></router-outlet>
+      <simon-game></simon-game>
     </main>
 
-    <pre>this.appState.state = {{ appState.state | json }}</pre>
-
     <footer>
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
     </footer>
   `
 })
-@RouteConfig([
-  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
-  { path: '/home',  name: 'Home',  component: Home },
-  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
-])
 export class App {
-  angularclassLogo = 'assets/img/angularclass-avatar.png';
-  name = 'Angular 2 Webpack Starter';
-  url = 'https://twitter.com/AngularClass';
-
-  constructor(public appState: AppState) {}
-
-  ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
---
+++ src/app/simon/SimonScore.ts
@@ -0,0 +1,58 @@
+import {Component, Input} from 'angular2/core';
+
+@Component({
+  selector: 'simon-score',
+  template: `
+    <div>
+      <span class="container">
+        8888
+        <span class="background">8888</span>
+        <span class="content">{{zeroPad(score, 4)}}</span>
+      </span>
+    </div>
+  `,
+  styles: [`
+    @font-face {
+      font-family: "D7MI";
+      src: url("./assets/fonts/DSEG7Modern-Italic.woff") format('woff');
+    }
+
+    div {
+      position: relative;
+      margin: 2pc;
+      font-size: 50px;
+      font-family: D7MI;
+    }
+
+    span.container {
+      position: relative;
+      color: transparent;
+    }
+
+    span {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    span.background {
+      color: #300;
+    }
+
+    span.content {
+      color: red;
+    }
+  `
+  ]
+})
+export class SimonScore {
+  @Input() score: number;
+
+  zeroPad(value: number, padding: number) {
+    let result = value.toString();
+    while (result.length < padding) {
+      result = '0' + result;
+    }
+    return result;
+  }
+}
