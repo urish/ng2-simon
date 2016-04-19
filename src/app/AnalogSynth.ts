32722141ab06ca021b5b98b58b570520b4073474
Uri Shaked <uri@salsa4fun.co.il>
2016-04-19 18:36:45 +0100

    fix(iot): handle the case where WebAudio is not available (such as on IoT platform)

--- src/app/AnalogSynth.ts
+++ src/app/AnalogSynth.ts
@@ -2,14 +2,24 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class AnalogSynth {
-  private context = new AudioContext();
+  private context: AudioContext = null;
+
+  constructor() {
+    if (typeof AudioContext !== 'undefined') {
+      this.context = new AudioContext();
+    } else {
+      console.log('Warning: Web Audio API is not available');
+    }
+  }
 
   public playTone(frequency: number, durationMs: number) {
+    if (this.context) {
       const osc = this.context.createOscillator();
       osc.type = 'sawtooth';
       osc.frequency.value = frequency;
       osc.connect(this.context.destination);
       osc.start(this.context.currentTime);
       osc.stop(this.context.currentTime + durationMs / 1000.);
+    }
   }
 }
