ede491b0a8055e4522eaef8c624ea2c298662850
Uri Shaked <uri@salsa4fun.co.il>
2016-04-19 20:01:14 +0100

    feat(iot): implement AnalogSynth and SevenSegment components

---
+++ src/app/iot/SevenSegment.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnChanges} from 'angular2/core';
+import {exec} from 'child_process';
+
+@Component({
+  'selector': 'iot-sevensegment',
+  'template': ''
+})
+export class SevenSegment implements OnChanges {
+  @Input() value: string | number = null;
+
+  ngOnChanges() {
+    if (this.value !== null) {
+      exec(`i2cset -y 1 0x71 0x76`);
+      console.log('on changes', this.value);
+      for (let letter of this.value.toString().substr(0, 4)) {
+        exec(`i2cset -y 1 0x71 ${letter}`);
+      }
+    }
+  }
+}
