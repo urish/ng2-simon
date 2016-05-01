664f45c54eaf2779999fcacf2b2b9846964a1119
Uri Shaked <uri@salsa4fun.co.il>
2016-05-01 12:41:12 -0600

    feat(simon): use common base class for IoT and Web audio synth
    thanks @pascalprecht for helping out

---
+++ src/app/web/WebAnalogSynth.ts
@@ -0,0 +1,36 @@
+import {Injectable} from 'angular2/core';
+import {AnalogSynth} from '../AnalogSynth';
+
+@Injectable()
+export class WebAnalogSynth extends AnalogSynth {
+  private context: AudioContext = null;
+
+  constructor() {
+    super();
+    if (typeof AudioContext !== 'undefined') {
+      this.context = new AudioContext();
+    } else {
+      console.log('Warning: Web Audio API is not available');
+    }
+  }
+
+  public playTone(frequency: number, durationMs: number) {
+    if (this.context) {
+      const osc = this.context.createOscillator();
+      osc.type = 'sawtooth';
+      osc.frequency.value = frequency;
+      osc.connect(this.context.destination);
+      osc.start(this.context.currentTime);
+      osc.stop(this.context.currentTime + durationMs / 1000.);
+    }
+  }
+
+  public playSound(path: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const soundFile = new Audio(path);
+      soundFile.play();
+      soundFile.onended = resolve;
+      soundFile.onerror = reject;
+    });
+  }
+}
