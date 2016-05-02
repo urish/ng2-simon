aa0e727ecef73c7f7579702e9ee7752ccd24e877
Uri Shaked <uri@salsa4fun.co.il>
2016-05-02 13:55:51 -0600

    feat(simon): wait for the player before starting the game

---
+++ src/app/simon/SimonBlink.ts
@@ -0,0 +1,28 @@
+import {Component, OnDestroy} from 'angular2/core';
+
+@Component({
+  selector: 'simon-blink',
+  template: `
+    <div [style.visibility]="visible?'visible':'hidden'">
+      <ng-content></ng-content>
+    </div>
+  `
+})
+export class SimonBlink implements OnDestroy {
+  private visible: boolean = true;
+  private timer: NodeJS.Timer;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.visible = !this.visible;
+    }, 500);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+}
